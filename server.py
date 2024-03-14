e003098d26f96b3a892f0f77ca0cda427947ea30
bchow3 <bchow3466@gmail.com>
2024-03-14 14:19:32 -0400

    created initial register route

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from flask import Flask, request, send_from_directory, make_response
+from flask import Flask, request, send_from_directory, make_response, redirect, url_for, jsonify
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -36,6 +36,15 @@ def function():
 @app.route('/style.css')
 def css():
     return send_from_directory('static', 'style.css')
+
+@app.route('/register',methods=["POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        return redirect(url_for("index"))
+    else:
+        return jsonify({'error': 'Method not allowed'}), 405
     
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
