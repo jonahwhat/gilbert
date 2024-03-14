bbbe01560bdf883dd65660fbdfd0d0ee7e5a3770
Jonah <105020079+jonahwhat@users.noreply.github.com>
2024-03-14 13:37:19 -0400

    Add initial auth token stuff

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from pymongo import MongoClient
 from flask import Flask, request, send_from_directory, make_response
 from markupsafe import escape
+from util.auth import *
 
 app = Flask(__name__)
 app.config['SESSION_COOKIE_HTTPONLY'] = True
 
+# setting up database
+mongo_client = MongoClient("mongo")
+db = mongo_client["cse312"]
+user_collection = db["user"]
+auth_collection = db["auth"]
 
 @app.after_request
 def add_security_headers(response):
@@ -12,23 +18,27 @@ def add_security_headers(response):
     return response
 
 
-
 @app.route('/')
 @app.route('/index')
 def index():
     return send_from_directory('public', 'index.html')
+
+
 # assumes database users with data id password and username per user
 @app.route('/login', methods=['POST'])
-def login():
+def login(response):
     user_data = request.json
     username = user_data['username']
     password = user_data['password']
     
     # Find the user by username
-    user = users_collection.find_one({'username': username})
+    user = user_collection.find_one({'username': username})
     
     if user and hash(password) == user['password']:
         session['user_id'] = str(user['_id'])
+
+    setAuthToken(response, username, auth_collection)
+    return response
 
 @app.route('/test')
 def testRoute():
---
+++ util/auth.py
@@ -0,0 +1,33 @@
+import uuid
+import hashlib
+
+# set this upon the user logging in
+def setAuthToken(response, username, auth_collection):
+    # HttpOnly directive set, give user auth token, store hash of token in db
+    token = str(uuid.uuid4())
+    response.addHeader(f"Set-Cookie: auth={token}; Max-Age=36000; HttpOnly")
+    
+    # You must store a hash (no salt) of each token in your database
+    hashed_token = hashlib.sha256(token.encode()).digest()
+    auth_token = {
+        "username": username,
+        "hashed_token": hashed_token
+    }
+    
+    auth_collection.insert_one(auth_token)
+
+
+# use for when the user logs out
+def removeAuthToken(request, response, auth_collection):
+    # get token
+    auth_token = request.cookies.get("auth", "")
+    hashed_token = hashlib.sha256(auth_token.encode()).digest()
+
+    # remove token from db
+    result = auth_collection.delete_one({"hashed_token": hashed_token})
+
+    # send response with empty auth token and expires directive
+    response.setResponseLine("HTTP/1.1 302 Found")
+    response.setContentType("application/json; charset=utf-8")
+    response.addHeader(f"Set-Cookie: auth=; Max-Age=1; HttpOnly")
+    response.addHeader("Location: /")
